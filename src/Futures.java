4c6f8ab2e4c504d8c90e150f10914f1ceeefc43b
kalyssao <kalyssaowusu@gmail.com>
2021-02-12 09:54:51 +0000

    price instead of value

--- src/Futures.java
+++ src/Futures.java
@@ -4,13 +4,15 @@ public class Futures extends Product {
     int month;
     int year;
 
-    Futures(String id, String exchange, String contractCode, int month, int year){
+    Futures(String id, String exchange, String contractCode, int month, int year,
+            ProductPricingService pricingService){
         super(id);
 
         this.exchange = exchange;
         this.contractCode = contractCode;
         this.month = month;
         this.year = year;
+        this.price = pricingService.price(exchange, contractCode, month, year);
     }
 
 }
--- src/Stocks.java
+++ src/Stocks.java
@@ -2,9 +2,10 @@ public class Stocks extends Product {
     String ticker;
     String exchange;
 
-    Stocks(String id, String ticker, String exchange){
+    Stocks(String id, String ticker, String exchange, ProductPricingService pricingService){
         super(id);
         this.ticker = ticker;
         this.exchange = exchange;
+        this.price = pricingService.price(exchange, ticker);
     }
 }
